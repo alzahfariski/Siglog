302d51aaba5bdc4255e0b5e6068291f56dac179d
alzahfariski <alzahfariski@gmail.com>
2023-05-10 22:56:07 +0700

    menambah dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
     @yield('css')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
